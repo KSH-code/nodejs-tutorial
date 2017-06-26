ce4ddd9f75e08100e7421a97ed35ed9c9d82b782
KSH-code <tjdgnsqn133@gmail.com>
2017-06-26 20:24:41 +0900

    Add files via upload

--- nodejs-tutorial/1/CreateHttpServer.js
+++ nodejs-tutorial/1/CreateHttpServer.js
@@ -1,13 +1,15 @@
 /**
  * Created by kimseonghoon on 2017-06-26.
+ * How to run?
+ * node CreateHttpServer.js
  */
 'use strict';
-const http = require('http');
+const http = require('http');//http moudle
 
 http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write('<h1>Hello World</h1>');
     res.end();
-}).listen(1234, () => {
+}).listen(1234, () => {//open 1234 port
     console.log('Server running at http://127.0.0.1:1234');
 });
--- nodejs-tutorial/1/HelloWorld.js
+++ nodejs-tutorial/1/HelloWorld.js
@@ -1,4 +1,6 @@
 /**
  * Created by kimseonghoon on 2017-06-26.
+ * How to run?
+ * node HelloWorld.js
  */
 console.log('Hello World');
