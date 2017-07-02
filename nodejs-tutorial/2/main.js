021ce1cbfc77d936f20fa5c41eb28ac6f6799aac
Kim SeongHoon <tjdgnsqn133@gmail.com>
2017-07-02 22:01:45 +0900

    Create "2" Folder

---
+++ nodejs-tutorial/2/main.js
@@ -0,0 +1,3 @@
+var module = require('./module');
+module.Hello('안녕하세요 인사입니다.');
+module.Bye('안녕히가세요 인사입니다.');
---
+++ nodejs-tutorial/2/process.js
@@ -0,0 +1,7 @@
+process.argv.forEach((item,index)=>{
+    if(item == '--exit'){//node process.js --exit Number
+        setTimeout(()=>{
+            process.exit();//Process exit
+        },Number(process.argv[index + 1]));//Number
+    }
+});
