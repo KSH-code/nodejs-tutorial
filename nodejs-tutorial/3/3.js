ff9b39bb5603128c8438bdc6e1f612f22e21cc6e
Kim SeongHoon <tjdgnsqn133@gmail.com>
2017-07-06 22:19:21 +0900

    3 folder
    http://ksh-code.tistory.com

---
+++ nodejs-tutorial/3/3.js
@@ -0,0 +1,12 @@
+/**
+ * @author [KSH-code]
+ * @email [tjdgnsqn133@gmail.com]
+ * @create date 2017-07-06 09:58:25
+ * @modify date 2017-07-06 09:58:25
+ * @desc [http response]
+*/
+var http = require('http');
+http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });//content-type setting
+    res.end('<h1>제목 입니다.</h1>');
+}).listen(1234);
---
+++ nodejs-tutorial/3/4.js
@@ -0,0 +1,20 @@
+/**
+ * @author [KSH-code]
+ * @email [tjdgnsqn133@gmail.com]
+ * @create date 2017-07-06 10:04:50
+ * @modify date 2017-07-06 10:04:50
+ * @desc [html file rendering]
+*/
+var http = require('http');
+var fs = require('fs');
+
+http.createServer((req, res) => {
+    fs.readFile('4-1.html', (e, data) => {
+        if (!e) {
+            res.writeHead(200, {
+                'Cotent-Type': 'text/html; charset=utf-8'
+            });
+            res.end(data);
+        }
+    });
+}).listen(1234);
